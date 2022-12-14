309447945b8e05e199fd9b780f01a3508bb7cab8
dmsxiagege <50934936+dmsxiagege@users.noreply.github.com>
2022-12-14 22:51:14 +0800

    Update and rename site.config.ts to site.config.js

--- site.config.js
+++ site.config.js
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'a5c87f726b3246bca90e97c3a03c49d9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '语航圆的主页',
+  domain: 'https://www.notion.so/a5c87f726b3246bca90e97c3a03c49d9',
+  author: 'dms',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  email: '3601094683@qq.com',
+//   github: 'transitive-bullshit',
+//   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
